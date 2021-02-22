8affc891470edb6aa8baeba7152d6f655971a709
William Chen <williamchen13225@gmail.com>
2021-02-22 12:28:34 -0800

    install react-router-dom package and implemented basic router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
 import Header from './components/Header.js';
 import Home from './components/Home.js';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="app">
-      <Header/>
-      <Home/>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+
+          <Route path="/checkout">
+            <Header/>
+            <h1> i am the checkout page </h1>
+          </Route>
+
+          <Route path="/">
+            <Header/>
+            <Home/>
+          </Route>
+
+        </Switch>
+      </div> 
+    </Router>
   );
 }
 
