564246b37d2f3c83e42924e50c9547b6fecf0fe4
William Chen <williamchen13225@gmail.com>
2021-03-03 17:15:10 -0800

    created getCartTotal higher order function to sync cart with the subtotal component

--- src/components/Subtotal.js
+++ src/components/Subtotal.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import './Subtotal.css';
 import CurrencyFormat from 'react-currency-format';
+import {useStateValue} from './StateProvider.js';
+import {getCartTotal} from './reducer.js';
 
 function Subtotal() {
+
+    const [{cart}, dispatch] = useStateValue();
+
     return (
         <div className='subtotal'>
             <CurrencyFormat
                 renderText={ (value)=> (
                     <>
-                        <p> Subtotal (0 items): <strong> 0 </strong> </p>
+                        <p> Subtotal ({cart.length} items): <strong> {value} </strong> </p>
                         <small className="subtotal_gift"> <input type="checkbox"/> This order contains a gift</small>
                     </>
                 ) } 
                 decimalScale={2}
-                value={0}
+                value={getCartTotal(cart)}
                 displayType={"text"}
                 thousandSeparator={true}
                 prefix={"$"}
--- src/components/reducer.js
+++ src/components/reducer.js
@@ -2,6 +2,12 @@ export const initialState = {
     cart:[],
 };
 
+//selector-getCartTotal
+
+export const getCartTotal = (cart) => (
+    cart?.reduce((amount, item) => amount + item.price, 0)
+);
+
 const reducer = (state, action) => {
     switch(action.type) {
         case "ADD_TO_CART":
