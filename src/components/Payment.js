b3aa8b1ebd853c5511ccd5fb543cd0fefeb70524
William Chen <williamchen13225@gmail.com>
2021-03-28 21:19:14 -0700

    added handleSubmit and handleChange fumctions and added axios.js file

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './Payment.css';
 import {useStateValue} from './StateProvider';
 import CheckoutProduct from './CheckoutProduct.js';
@@ -6,6 +6,8 @@ import {Link} from 'react-router-dom';
 import {CardElement, useStripe, useElements} from '@stripe/react-stripe-js';
 import CurrencyFormat from 'react-currency-format';
 import {getCartTotal} from './reducer.js'
+import axios from './axios';
+import {useHistory} from 'react-router-dom';
 
 function Payment() {
 
@@ -13,15 +15,43 @@ function Payment() {
     
     const stripe = useStripe();
     const elements = useElements();
+    const history = useHistory();
 
     //state
     const [error, setError] = useState(null);
     const [disabled, setDisabled] = useState(true);
-    const [processing, setProcessing] = useState(false);
+    const [processing, setProcessing] = useState("");
     const [succeeded, setSucceeded] = useState(false);
+    const [clientSecret, setClientSecret] = useState('');
 
-    const handleSubmit = e => {
+    useEffect(()=>{
+        const getClientSecret = async () => {
+            const response = await axios({
+                method: 'post', 
+                url: `/payments/create?total=${getCartTotal(cart)*100}`
+        })
+        setClientSecret(response.data.clientSecret);
+    }
+    getClientSecret();
+    } 
+    ,[cart]);
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
+        setProcessing(true);
+
+        const payload = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: elements.getElement(CardElement)
+            }
+        }).then(({paymentIntent})=> {
+            setProcessing(false);
+            setError(null);
+            setSucceeded(true);
+
+            history.replace('/');
+        })
+
     }
 
     const handleChange = e => {
@@ -81,6 +111,9 @@ function Payment() {
                                 </span>
                             </button>
                         </div>
+                        
+                       {/*fail safe for errors */}
+                        {error && <div>{error}</div>}
                     </form>
 
                 </div>
---
+++ src/components/axios.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseUrl: '...'
+});
+
+export default instance;
+
