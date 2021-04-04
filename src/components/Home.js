54d4ff38cb6562faf9750319f0000c8a93aa704b
William Chen <williamchen13225@gmail.com>
2021-04-04 04:28:05 -0700

    render home page data from firestore

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,61 +1,82 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Home.css';
 import Product from './Product.js';
 import { v4 as uuidv4 } from 'uuid';
+import {db} from '../firebase.js';
 
 function Home() {
+
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(()=> {
+        db.collection('products')
+        .orderBy('priority', 'asc')
+        .onSnapshot(snapshot => {
+            setProducts(snapshot.docs.map(doc => ({
+                id: doc.id,
+                title: doc.data().title,
+                price: doc.data().price,
+                rating: doc.data().rating,
+                image: doc.data().image,
+            })))
+        })
+    }, [])
+
+
     return (
-        <div className="home">
+        products.length !== 0 && <div className="home">
             <div className="home_container">
                 <img className="home_image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt="banner"/>
                 <div className="home_row">
                     <Product
-                        id={uuidv4()}
-                        title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
-                        price={19.99}
-                        rating={5}
-                        image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+                        id={products[0].id}
+                        title={products[0].title}
+                        price={products[0].price}
+                        rating={products[0].rating}
+                        image={products[0].image}
                     />
+                
                     <Product
-                        id={uuidv4()}
-                        title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
-                        price={239.0}
-                        rating={4}
-                        image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
-                    />
-                </div>
-
-                <div className="home_row">
-                    <Product
-                        id={uuidv4()}
-                        title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
-                        price={199.99}
-                        rating={3}
-                        image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
-                    />
-                    <Product
-                        id={uuidv4()}
-                        title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
-                        price={98.99}
-                        rating={5}
-                        image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
-                    />
-                    <Product
-                        id={uuidv4()}
-                        title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
-                        price={598.99}
-                        rating={4}
-                        image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+                        id={products[1].id}
+                        title={products[1].title}
+                        price={products[1].price}
+                        rating={products[1].rating}
+                        image={products[1].image}
                     />
                 </div>
 
                 <div className="home_row">
                     <Product
-                        id={uuidv4()}
-                        title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
-                        price={1094.98}
-                        rating={4}
-                        image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
+                        id={products[2].id}
+                        title={products[2].title}
+                        price={products[2].price}
+                        rating={products[2].rating}
+                        image={products[2].image}
+                    />
+                    <Product
+                        id={products[3].id}
+                        title={products[3].title}
+                        price={products[3].price}
+                        rating={products[3].rating}
+                        image={products[3].image}
+                    />
+                    <Product
+                        id={products[4].id}
+                        title={products[4].title}
+                        price={products[4].price}
+                        rating={products[4].rating}
+                        image={products[4].image}
+                    />
+                </div>
+
+                <div className="home_row">
+                    <Product
+                        id={products[5].id}
+                        title={products[5].title}
+                        price={products[5].price}
+                        rating={products[5].rating}
+                        image={products[5].image}
                     />
                 </div>
             </div>
